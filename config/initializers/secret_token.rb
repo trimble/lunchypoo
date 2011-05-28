689d37697fb52aeb84d25246bb2a9240473c3f30
Todd Trimble <todd.trimble@gmail.com>
2011-05-28 12:37:21 -0400

    Relaunch of lunchypoo development. A new chapter begins, the same question is answered by a new technology, more bellies are happy.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchypoo::Application.config.secret_token = '0977f46ed0fe8ed71cb5a52a47ade8bb3635f6a4cb736983d0c9e7b7b3591f3c8c7736238078c1f6518a06f6ac76641394a91116cde6e9d47a5b50316dab8ff1'
